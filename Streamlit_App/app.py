8225dd8412d92b67e106a3772a7ddb8ebc4f6184
Vivek Pathak <vp6706951@gmail.com>
2026-07-13 16:22:38 +0530

    frontend change

--- Streamlit_App/app.py
+++ Streamlit_App/app.py
@@ -8,6 +8,9 @@ MODEL_DIR = BASE_DIR.parent / "models"
 model = joblib.load(MODEL_DIR / "Random_Forest_House_Price.pkl")
 columns = joblib.load(MODEL_DIR / "columns.pkl")
 
+# ============================================================
+# PAGE CONFIG
+# ============================================================
 st.set_page_config(
     page_title="House Price Prediction",
     page_icon="🏠",
@@ -19,7 +22,7 @@ st.write("Enter the house details below to predict its price.")
 
 # ---------------- Numerical Inputs ----------------
 
-area = st.number_input("Area (sq.ft)", min_value=200, max_value=20000, value=5000)
+area = st.number_input("Area (sq.ft)", min_value=500, max_value=20000, value=5000)
 
 bedrooms = st.selectbox("Bedrooms", [1,2,3,4,5,6])
 
@@ -45,29 +48,27 @@ furnishingstatus = st.selectbox(
 # ---------------- Prediction ----------------
 
 if st.button("Predict Price"):
-    if area < 200:
-        st.warning("⚠️ Area seems too small. Please enter a value of at least 200 sq.ft.")
-    else:
-        data = {
-            "area":[area],
-            "bedrooms":[bedrooms],
-            "bathrooms":[bathrooms],
-            "stories":[stories],
-            "parking":[parking],
-            "mainroad":[mainroad],
-            "guestroom":[guestroom],
-            "basement":[basement],
-            "airconditioning":[airconditioning],
-            "prefarea":[prefarea],
-            "furnishingstatus":[furnishingstatus]
-        }
+
+    data = {
+        "area":[area],
+        "bedrooms":[bedrooms],
+        "bathrooms":[bathrooms],
+        "stories":[stories],
+        "parking":[parking],
+        "mainroad":[mainroad],
+        "guestroom":[guestroom],
+        "basement":[basement],
+        "airconditioning":[airconditioning],
+        "prefarea":[prefarea],
+        "furnishingstatus":[furnishingstatus]
+    }
 
         df = pd.DataFrame(data)
         df = pd.get_dummies(df)
         df = df.reindex(columns=columns, fill_value=0)
 
-        prediction = model.predict(df)[0]
+    prediction = model.predict(df)[0]
 
-        st.success(f"🏡 Estimated House Price: ₹ {prediction:,.0f}")
+    st.success(f"🏡 Estimated House Price: ₹ {prediction:,.0f}")
 
 
